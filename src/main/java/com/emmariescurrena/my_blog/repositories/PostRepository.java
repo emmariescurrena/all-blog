3822c3556cd9322daa377107803a0ac8ea3a95c3
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-11 08:03:03 -0300

    Adds Repository annotation

--- src/main/java/com/emmariescurrena/my_blog/repositories/PostRepository.java
+++ src/main/java/com/emmariescurrena/my_blog/repositories/PostRepository.java
@@ -1,9 +1,11 @@
 package com.emmariescurrena.my_blog.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.my_blog.models.Post;
 
+@Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
     Post findByUrl(String url);
 }
--- src/main/java/com/emmariescurrena/my_blog/repositories/UserRepository.java
+++ src/main/java/com/emmariescurrena/my_blog/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.emmariescurrena.my_blog.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.my_blog.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
