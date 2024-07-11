9821252702b0f4b368c925861d8f3dd29c7ffe3d
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-11 08:17:20 -0300

    Change repositories type from CrudRepository to JpaRepository

--- src/main/java/com/emmariescurrena/my_blog/repositories/PostRepository.java
+++ src/main/java/com/emmariescurrena/my_blog/repositories/PostRepository.java
@@ -1,11 +1,11 @@
 package com.emmariescurrena.my_blog.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.my_blog.models.Post;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByUrl(String url);
 }
--- src/main/java/com/emmariescurrena/my_blog/repositories/UserRepository.java
+++ src/main/java/com/emmariescurrena/my_blog/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.emmariescurrena.my_blog.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.my_blog.models.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
