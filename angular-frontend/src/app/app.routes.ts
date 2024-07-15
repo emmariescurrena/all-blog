60c7b959e02bccfed747d6165ce6ed7e61eb1256
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-15 17:00:37 -0300

    Adds userlist route

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from '../model/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'userlist', component: UserListComponent },
+];
