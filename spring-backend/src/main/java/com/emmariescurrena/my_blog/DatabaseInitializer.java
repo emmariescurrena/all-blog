a98a851a15baad01ccb4611102f8f0ad16bc3527
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-15 14:58:31 -0300

    Adds other user and post

--- spring-backend/src/main/java/com/emmariescurrena/my_blog/DatabaseInitializer.java
+++ spring-backend/src/main/java/com/emmariescurrena/my_blog/DatabaseInitializer.java
@@ -22,12 +22,19 @@ public class DatabaseInitializer implements CommandLineRunner {
     public void run(String... args) {
 
         userDAO.save(new User("emmanuel", "$2a$10$01YESQmWVy4Xv2CCUx08sO.M1Ge5RRlOqp7zFUC9xxzTZaxJvVPq2"));
+        userDAO.save(new User("roberto", "$2a$10$BFUSAWDrROkY6XRpBJbf3OyRiW/or2Wo1OqOI3fyXY7PfXMQ4Rm8."));
 
         postDAO.save(
                 new Post("this-is-the-title",
                         "This is the title",
                         "# Heading\n ## Sub-heading\nBody",
                         userDAO.findById(1L).get()));
+
+        postDAO.save(
+                new Post("this-is-the-title-baby",
+                        "This is the title, baby",
+                        "# Heading\n ## Sub-heading\nBody",
+                        userDAO.findById(2L).get()));
 
         System.out.println("Database initialized!");
 
