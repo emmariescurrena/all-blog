64de4ac04e85375d4bc64fa3f34ec7737be3e2b4
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-16 20:16:43 -0300

    Reset project

--- angular-frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserListComponent } from '../model/user-list/user-list.component';
-
-export const routes: Routes = [
-    { path: 'userlist', component: UserListComponent },
-];
