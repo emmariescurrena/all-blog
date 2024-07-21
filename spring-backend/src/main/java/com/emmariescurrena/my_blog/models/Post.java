d3d785e4b33614e5160bedabfbf59092dd8fc142
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-21 09:32:27 -0300

    Adds content to post and connects post with user

--- spring-backend/src/main/java/com/emmariescurrena/my_blog/models/Post.java
+++ spring-backend/src/main/java/com/emmariescurrena/my_blog/models/Post.java
@@ -1,5 +1,36 @@
 package com.emmariescurrena.my_blog.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "POSTS")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
 }
--- spring-backend/src/main/java/com/emmariescurrena/my_blog/models/User.java
+++ spring-backend/src/main/java/com/emmariescurrena/my_blog/models/User.java
@@ -12,10 +12,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -41,6 +43,9 @@ public class User implements UserDetails{
     @Column(nullable = false)
     @JsonIgnore
     private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
 
     @OneToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
