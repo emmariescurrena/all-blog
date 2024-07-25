bc8dfe30145b0d11f3cd5966415658e5d4434d70
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-25 12:47:20 -0300

    Adds validation

--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/UpdateUserEmailDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/UpdateUserEmailDto.java
@@ -1,12 +1,19 @@
 package com.emmariescurrena.all_blog.dtos;
 
+import com.emmariescurrena.all_blog.util.RegexValidator;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UpdateUserEmailDto {
 
-    private String oldEmail;
-
+    @NotEmpty(message = "The new email is required")
+    @Email(regexp = RegexValidator.EMAIL,
+            flags = Pattern.Flag.CASE_INSENSITIVE,
+            message = "Invalid email format")
     private String newEmail;
 
 }
--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/UpdateUserPasswordDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/UpdateUserPasswordDto.java
@@ -1,12 +1,24 @@
 package com.emmariescurrena.all_blog.dtos;
 
+import com.emmariescurrena.all_blog.validators.ValidPassword;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class UpdateUserPasswordDto {
-    private String oldPassword;
 
+    @NotEmpty(message = "The new password is required")
+    @ValidPassword
     private String newPassword;
 
+    @NotEmpty(message = "Confirm new password must be equal to new password")
     private String confirmNewPassword;
+
+    @AssertTrue(message = "Passwords do not match")
+    private boolean isPasswordMatch() {
+        return newPassword != null && newPassword.equals(confirmNewPassword);
+    }
+
 }
