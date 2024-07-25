34c7a44f8c28a95f3abf3892542f4688892145db
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-25 13:00:50 -0300

    Adds validation

--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/CreatePostDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/CreatePostDto.java
@@ -1,12 +1,20 @@
 package com.emmariescurrena.all_blog.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreatePostDto {
+    
+    @NotEmpty(message = "The title is required")
+    @Size(min = 2, max = 256, message = "The length of title must be between 2 and 256 characters")
     private String title;
     
+    @NotEmpty(message = "The body is required")
+    @Size(min = 50, max = 20000, message = "The length of body must be between 50 and 20000 characters")
     private String body;
 
+    @NotEmpty(message = "The user id is required")
     private Long userId;
 }
