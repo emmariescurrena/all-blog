59e70a92e47b794dc6333d249e7f874dcf63a359
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-07-29 11:11:52 -0300

    Merge Update and Create Dtos into one

--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/PostDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/PostDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class CreatePostDto {
+public class PostDto {
     
     @NotEmpty(message = "The title is required")
     @Size(min = 2, max = 256, message = "The length of title must be between 2 and 256 characters")
@@ -15,6 +15,4 @@ public class CreatePostDto {
     @Size(min = 50, max = 20000, message = "The length of body must be between 50 and 20000 characters")
     private String body;
 
-    @NotEmpty(message = "The user id is required")
-    private Long userId;
 }
