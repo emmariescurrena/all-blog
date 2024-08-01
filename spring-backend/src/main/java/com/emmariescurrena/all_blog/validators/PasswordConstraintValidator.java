306a5b79e56aca7bc72fc20b24d795f4c04a2d8b
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-01 12:02:03 -0300

    Groups all different errors into one less verbose string

--- spring-backend/src/main/java/com/emmariescurrena/all_blog/validators/PasswordConstraintValidator.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/validators/PasswordConstraintValidator.java
@@ -1,8 +1,6 @@
 package com.emmariescurrena.all_blog.validators;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
@@ -16,7 +14,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
-    
+
     @Override
     public void initialize(ValidPassword arg0) {
     }
@@ -25,25 +23,35 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
     public boolean isValid(String password, ConstraintValidatorContext context) {
 
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
-            new LengthRule(8, 30),
-            new CharacterRule(EnglishCharacterData.UpperCase, 1),
-            new CharacterRule(EnglishCharacterData.LowerCase, 1),
-            new CharacterRule(EnglishCharacterData.Digit, 1),
-            new CharacterRule(EnglishCharacterData.Special, 1),
-            new WhitespaceRule()
-        ));
+                new LengthRule(8, 30),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new WhitespaceRule()));
 
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
             return true;
         }
-        List<String> messages = validator.getMessages(result);
 
-        String messageTemplate = messages.stream()
-            .collect(Collectors.joining(","));
-        context.buildConstraintViolationWithTemplate(messageTemplate)
-            .addConstraintViolation()
-            .disableDefaultConstraintViolation();
+        /*
+         * List<String> messages = validator.getMessages(result);
+         * 
+         * String messageTemplate = messages.stream()
+         * .collect(Collectors.joining(","));
+         */
+
+        context.buildConstraintViolationWithTemplate(
+                String.join("",
+                        "Password must have at least 1 lowercased letter, ",
+                        "1 uppercased letter, ",
+                        "1 number, ",
+                        "and 1 special character, ",
+                        "a min. of 8 characters ",
+                        "and a max. of 30"))
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
         return false;
 
     }
