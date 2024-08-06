479701029df2b2e2fb054c18c93341a671cc7fb0
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-06 09:12:11 -0300

    Adds user route

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
     { path: "register", component: RegisterComponent },
     { path: "login", component: LoginComponent },
     { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
-    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "users/:id", component: UserComponent, canActivate: [AuthGuard] },
 ];
