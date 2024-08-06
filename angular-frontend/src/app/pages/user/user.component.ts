0e6ff4c71d65a124c1ffb49546e9a54975bac9c3
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-06 09:13:12 -0300

    Creates user service with getUser and UserComponent

---
+++ angular-frontend/src/app/pages/user/user.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user-service/user.service';
+import { User } from '../../models/user/user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'app-user',
+    standalone: true,
+    imports: [],
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.scss'
+})
+export class UserComponent implements OnInit {
+
+    public user = new User();
+
+    constructor(
+        private userService: UserService,
+        private route: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            const id = params['id'];
+            this.loadUser(id);
+        });
+    }
+
+    loadUser(id: number) {
+        this.userService.getUser(id).subscribe({
+            next: res => {
+                this.user = res;
+            },
+            error: e => console.log(e)
+        });
+    }
+}
--- angular-frontend/src/app/services/user-service/user.service.ts
+++ angular-frontend/src/app/services/user-service/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BACKEND_URL } from '../../consts/url.constants';
+import { User } from '../../models/user/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    constructor(
+        private httpClient: HttpClient,
+    ) { }
+
+    getUser(id: number) {
+        return this.httpClient.get<User>(`${BACKEND_URL}/users/${id}`);
+    }
 }
