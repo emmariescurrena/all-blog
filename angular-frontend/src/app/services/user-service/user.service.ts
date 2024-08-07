e611800ada2e403dcc4e67af4df8945049031908
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-07 07:54:11 -0300

    Adds getUserByEmail and getAllUsers

--- angular-frontend/src/app/services/user-service/user.service.ts
+++ angular-frontend/src/app/services/user-service/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BACKEND_URL } from '../../consts/url.constants';
 import { User } from '../../models/user/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +13,15 @@ export class UserService {
         private httpClient: HttpClient,
     ) { }
 
-    getUser(id: number) {
-        return this.httpClient.get<User>(`${BACKEND_URL}/users/${id}`);
+    getUserById(id: number): Observable<User> {
+        return this.httpClient.get<User>(`${BACKEND_URL}/users/byId/${id}`);
+    }
+
+    getUserByEmail(email: string): Observable<User> {
+        return this.httpClient.get<User>(`${BACKEND_URL}/users/byEmail/${email}`);
+    }
+
+    getAllUsers(): Observable<User[]> {
+        return this.httpClient.get<User[]>(`${BACKEND_URL}/users`);
     }
 }
