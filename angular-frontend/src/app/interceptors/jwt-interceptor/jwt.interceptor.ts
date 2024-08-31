3020214420f254ac67647e14da175ed7f18207cd
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-31 15:19:51 -0300

    Adds verification to check if url is an api request

--- angular-frontend/src/app/interceptors/jwt-interceptor/jwt.interceptor.ts
+++ angular-frontend/src/app/interceptors/jwt-interceptor/jwt.interceptor.ts
@@ -1,18 +1,20 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Observable } from 'rxjs';
-import { JwtService } from '../../services/jwt-service/jwt.service';
 import { AuthService } from '../../services/auth-service/auth.service';
+import { BACKEND_URL } from '../../consts/url.constants';
 
 export function JwtInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
 
-    if (inject(AuthService).isLoggedIn()) {
-        const token = inject(JwtService).getToken();
-        req = req.clone({
-            headers: req.headers.set("Authorization", `Bearer ${token}`)
-        });
+    const isApiUrl = req.url.startsWith(BACKEND_URL);
+    const isLoggedIn = inject(AuthService).isLoggedIn();
+    if (!isApiUrl || !isLoggedIn) {
         return next(req);
     }
-
+    const token = inject(AuthService).userValue?.token;
+    req = req.clone({
+        headers: req.headers.set("Authorization", `Bearer ${token}`)
+    });
     return next(req);
+
 }
