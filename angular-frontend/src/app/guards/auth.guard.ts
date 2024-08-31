4b139738d6cac76107b6afbc0cf51ed70e9e55a4
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-31 15:19:07 -0300

    Modifies to match user Subject , check for roles and saves url to return after login

--- angular-frontend/src/app/guards/auth.guard.ts
+++ angular-frontend/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth-service/auth.service';
 
 @Injectable({
@@ -10,12 +10,17 @@ export class AuthGuard implements CanActivate {
         private authService: AuthService,
         private router: Router) { }
 
-    canActivate(): boolean {
-        if (this.authService.isLoggedIn()) {
-            return true;
-        } else {
-            this.router.navigate(["/login"]);
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const user = this.authService.userValue;
+        if (!user || !this.authService.isLoggedIn()) {
+            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
             return false;
         }
+        const { roles } = route.data;
+        if (roles && !roles.includes(user.role)) {
+            this.router.navigate(['/']);
+            return false;
+        }
+        return true;
     }
 }
