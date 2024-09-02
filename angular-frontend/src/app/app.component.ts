0b28acd335241faf7b39ca86ea417776ad95ab3b
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-09-02 12:01:44 -0300

    Updates navbar depending of user logged or not

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Output } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { User } from './models/user/user';
+import { AuthService } from './services/auth-service/auth.service';
 
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     title = 'all-blog';
+    @Output() public user: User | null | undefined;
+
+    constructor(
+        private authService: AuthService,
+    ) {
+        this.authService.user.subscribe(user => this.user = user);
+    }
+
 }
