7cea6e7c8b421dec6ca1d47495b97918522b776d
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-09-04 10:36:33 -0300

    Adds posts loading to home

--- angular-frontend/src/app/pages/home/home.component.ts
+++ angular-frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post-service/post.service';
+import { Post } from '../../models/post/post';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+    public loading = false;
+    public posts: Post[] = [];
+
+    constructor(
+        private postService: PostService
+    ) { }
+
+    ngOnInit() {
+        this.loading = true;
+        this.postService.getAllPosts().subscribe({
+            next: posts => {
+                this.posts = posts;
+                this.loading = false;
+            },
+            error: e => console.log(e)
+        });
+    }
 }
