ae6637100577683001c28d98a3ca16d7a25d4f1f
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-09-04 10:35:12 -0300

    Makes fields final

--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/PostDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/PostDto.java
@@ -9,10 +9,10 @@ public class PostDto {
     
     @NotEmpty(message = "The title is required")
     @Size(min = 2, max = 256, message = "The length of title must be between 2 and 256 characters")
-    private String title;
+    private final String title;
     
     @NotEmpty(message = "The body is required")
     @Size(min = 50, max = 20000, message = "The length of body must be between 50 and 20000 characters")
-    private String body;
+    private final String body;
 
 }
--- spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/RegisterUserDto.java
+++ spring-backend/src/main/java/com/emmariescurrena/all_blog/dtos/RegisterUserDto.java
@@ -18,27 +18,27 @@ public class RegisterUserDto {
     @Email(regexp = RegexValidator.EMAIL,
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Invalid email format")
-    private String email;
+    private final String email;
 
 
     @NotEmpty(message = "The password is required")
     @ValidPassword
-    private String password;
+    private final String password;
 
     @NotEmpty(message = "Confirm password must be equal to password")
-    private String confirmPassword;
+    private final String confirmPassword;
 
     @AssertTrue(message = "Passwords do not match")
     private boolean isPasswordMatch() {
         return password != null && password.equals(confirmPassword);
     }
 
-    @NotEmpty(message = "The name is required")
+	@NotEmpty(message = "The name is required")
     @Size(min = 2, max = 100, message = "The length of name must be between 2 and 100 characters")
-    private String name;
+    private final String name;
 
     @NotEmpty(message = "The surname is required")
     @Size(min = 2, max = 100, message = "The length of surname must be between 2 and 100 characters")
-    private String surname;
+    private final String surname;
 
 }
