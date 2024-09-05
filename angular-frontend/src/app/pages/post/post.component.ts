baf42c805e7074ed385e40de8e737b5f57982def
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-09-05 10:26:45 -0300

    Moves post load to ngOnInit

--- angular-frontend/src/app/pages/post/post.component.ts
+++ angular-frontend/src/app/pages/post/post.component.ts
@@ -15,13 +15,12 @@ import { AuthService } from '../../services/auth-service/auth.service';
 export class PostComponent {
     public loading = false;
     public user: User | null;
-    public post: Post | null;
+    public post!: Post;
 
     constructor(
         private postService: PostService,
         private route: ActivatedRoute
     ) {
-        this.post = null;
         this.user = inject(AuthService).userValue;
     }
 
@@ -29,17 +28,14 @@ export class PostComponent {
         this.loading = true;
         this.route.params.subscribe(params => {
             const id = params["id"];
-            this.loadPost(id);
+            this.postService.getPost(id).subscribe({
+                next: post => {
+                    this.post = post;
+                    this.loading = false;
+                },
+                error: e => console.log(e)
+            });
         });
     }
 
-    loadPost(id: number) {
-        this.postService.getPost(id).subscribe({
-            next: post => {
-                this.post = post;
-                this.loading = false;
-            },
-            error: e => console.log(e)
-        });
-    }
 }
